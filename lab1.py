63fa3387328d3730b541acc4632f0a0db9fc3701
zhanhe778 <1925382415@qq.com>
2025-06-01 13:50:59 +0800

    修复TextGraph导入问题

--- lab1.py
+++ lab1.py
@@ -7,14 +7,8 @@ import networkx as nx
 import msvcrt  # Windows-specific module for keyboard input
 import time
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-#zhanhe
 
->>>>>>> C4
-=======
->>>>>>> B2
+
 class TextGraph:
     def __init__(self):
         self.graph = defaultdict(dict)  # Adjacency list representation
@@ -385,12 +379,10 @@ def main():
 if __name__ == "__main__":
     main()
 <<<<<<< HEAD
-<<<<<<< HEAD
-    #   D i f f e r e n t   c h a n g e  
+    #   D i f f e r e n t   c h a n g e 
+ 
 =======
-    #   T e s t   c h a n g e  
+    #   T e s t   c h a n g e 
+ 
 >>>>>>> C4
-=======
-    #   D i f f e r e n t   c h a n g e  
->>>>>>> B2
  
